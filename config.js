97c29bfc1accc8a2f65870bb80c6ae7799b258b1
Aaron <63269557+aaronspeaks94@users.noreply.github.com>
2020-08-28 01:00:31 -0700

    Passport Facebook

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl' : 'mongodb://localhost:27017/nucampsite',
+    'facebook' : {
+        clientId: '343501850165741',
+        clientSecret: 'c1dfca3ad7b1fb0713ca87f2426fdd14'
+    }
 }
